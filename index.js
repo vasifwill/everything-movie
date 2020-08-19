a0837c408853245cd53dfc40acbff1bb432ecd96
vasif mammadov <vasif.will@gmail.com>
2020-08-18 21:29:30 -0400

    Setting handlers

---
+++ index.js
@@ -0,0 +1,48 @@
+const Alexa = require('ask-sdk-core')
+const axios = require('axios')
+
+
+function getDirector (title){
+    const apikey = process.env.OMDB_API_KEY
+
+    return axios.get()
+}
+const DirectorIntentHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'DirectorIntent'
+    },
+    handle(handlerInput) {
+      const title = handlerInput.requestEnvelope.request.intent.slots.Title.value
+      const answer = `The director of ${title} is me. HAHA! Just kidding!`
+      const reprompt = 'Would you like information on another movie?'
+      return handlerInput.responseBuilder
+        .speak(answer + reprompt)
+        .reprompt(reprompt)
+        .withShouldEndSession(false)
+        .getResponse();
+    },
+  }
+
+  const ErrorHandler = {
+      canHandle() {
+          return true
+      },
+      handle(handlerInput) {
+          return handlerInput.responseBuilder
+          .speak("My bad, didn't catch that. Can you repeat it ?")
+          .reprompt("My bad, didn't catch that")
+      }
+  }
+
+  const builder = Alexa.SkillBuilders.custom()
+  exports.handler = builder
+    .addRequestHandlers(DirectorIntentHandler)
+    .addErrorHandlers(ErrorHandler)
+    .lambda()
+
+
+
+
+
+
